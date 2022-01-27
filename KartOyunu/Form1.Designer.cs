b715d6d0a52a9c73da312fc8024d0a40264936dd
Esra Alkan <94802109+esraalkn@users.noreply.github.com>
2022-01-27 14:29:50 +0300

    Add project files.

---
+++ KartOyunu/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace KartOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKaristir = new System.Windows.Forms.Button();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LBLscore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKaristir
+            // 
+            this.btnKaristir.Location = new System.Drawing.Point(266, 12);
+            this.btnKaristir.Name = "btnKaristir";
+            this.btnKaristir.Size = new System.Drawing.Size(216, 77);
+            this.btnKaristir.TabIndex = 0;
+            this.btnKaristir.Text = "Karıştır";
+            this.btnKaristir.UseVisualStyleBackColor = true;
+            this.btnKaristir.Click += new System.EventHandler(this.btnKaristir_Click);
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Location = new System.Drawing.Point(266, 324);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(216, 77);
+            this.btnGoster.TabIndex = 1;
+            this.btnGoster.Text = "Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(539, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SCORE :";
+            // 
+            // LBLscore
+            // 
+            this.LBLscore.AutoSize = true;
+            this.LBLscore.Location = new System.Drawing.Point(590, 44);
+            this.LBLscore.Name = "LBLscore";
+            this.LBLscore.Size = new System.Drawing.Size(0, 13);
+            this.LBLscore.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::KartOyunu.Properties.Resources.pngegg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(793, 413);
+            this.Controls.Add(this.LBLscore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.btnKaristir);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKaristir;
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LBLscore;
+    }
+}
+
---
+++ KartOyunu/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KartOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        PictureBox kart1 = new PictureBox();
+        PictureBox kart2 = new PictureBox();
+        private string[] kartliste = Directory.GetFiles(@"C:\Users\Lenovo\OneDrive\Masaüstü\Projeler\KartOyunu\kartlar");
+        string[] kartdizi1 = new string[26];
+        string[] kartdizi2 = new string[26];
+
+        int sayi = 0;
+        int score = 0;
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+
+            if (kartdizi1[sayi][61]==kartdizi2[sayi][61])
+            {
+                score++;
+                LBLscore.Text = score.ToString();
+            }
+
+            kart1.ImageLocation = kartdizi1[sayi];
+            kart1.Location = new Point(50, 120);
+            kart1.Size = new Size(100, 150);
+            kart1.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Controls.Add(kart1);
+
+            kart2.ImageLocation = kartdizi2[sayi];
+            kart2.Location = new Point(570, 120);
+            kart2.Size = new Size(100, 150);
+            kart2.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Controls.Add(kart2);
+            sayi++;
+            if (sayi==25)
+            {
+                MessageBox.Show("Game Over!");
+                sayi = 0;
+            }
+        }
+
+        private void btnKaristir_Click(object sender, EventArgs e)
+        {
+            kartliste = Karistirr(kartliste);
+            kartikiyebol(kartliste);
+        }
+
+        private void kartikiyebol(string[] kartliste)
+        {
+            for (int i = 0; i < kartliste.Length/2; i++)
+            {
+                kartdizi1[i] = kartliste[i];
+            }
+
+            int x = 0;
+
+            for (int i = 26; i < kartliste.Length; i++)
+            {
+                kartdizi2[x] = kartliste[i];
+                x++;
+            }
+        }
+
+        private string[] Karistirr(string[] kartliste)
+        {
+            string[] cekilenkart = new string[kartliste.Length];
+            Random rnd = new Random();
+            for (int i = 0; i < kartliste.Length; i++)
+            {
+                string kart = kartliste[rnd.Next(0, kartliste.Length)];
+                if (!cekilenkart.Contains(kart))
+                {
+                    cekilenkart[i] = kart;
+                }
+                else
+                {
+                    i--;
+                    continue;
+                }
+                
+            }
+
+            return cekilenkart ;
+        }
+    }
+}
